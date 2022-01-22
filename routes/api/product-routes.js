6f62da44859a244f0e3374044e2b0a5ba824f3b2
austin espinal <austinespi@gmail.com>
2022-01-21 19:06:35 -0500

    updated all routes to match mock-up

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -16,6 +16,10 @@ router.get('/', (req, res) => {
       {
         model: Tag,
         attributes: ['id', 'tag_name']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id', 'product_id', 'tag_id']
       }
     ]
   })
@@ -42,6 +46,10 @@ router.get('/:id', (req, res) => {
       {
         model: Tag,
         attributes: ['id', 'tag_name']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id', 'product_id', 'tag_id']
       }
     ]
   })
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -11,6 +11,10 @@ router.get('/', (req, res) => {
       {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id', 'product_id', 'tag_id']
       }
     ]
   })
@@ -32,6 +36,10 @@ router.get('/:id', (req, res) => {
       {
         model: Product,
         attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      },
+      {
+        model: ProductTag,
+        attributes: ['id', 'product_id', 'tag_id']
       }
     ]
   })
